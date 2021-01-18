e4e2e4a152d38ef46cd3174301f983bd5b2aba1e
ahm3d02 <57738969+ahm3d02@users.noreply.github.com>
2021-01-17 22:06:49 -0500

    Cleaning up code and replacing time
    Co-Authored-By: Luigi Quattrociocchi <luigi@quattrociocchi.net>

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 console.log('server running');
 const env = require("dotenv").config({ path: "./.env" });
 
-// // mongo
-// let mongojs = require("mongojs");
-// // let uri = 'mongodb://admin:root123@ds063449.mlab.com:63449/heroku_wtqs46qf';
-// let uri = 'mongodb://admin:password1@ds261238.mlab.com:61238/heroku_xk7d8sdw'
-// let db = mongojs(process.env.MONGODB_URI || uri);
-
 // mongodb atlas
 const Mongoose = require('mongoose');
 
@@ -14,8 +8,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 ;(async() => {
 	await Mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 	console.log("db connected");
-
-	// await test()
 })()
 
 const userSchema = new Mongoose.Schema({
@@ -27,23 +19,13 @@ const userSchema = new Mongoose.Schema({
 });
 const Account = Mongoose.model('Account', userSchema);
 
-// const test = async () => {
-// 	console.log("test")
-// 	await Account.find({username: "luigi"})
-// 	.then(entries => {
-// 	})
-// 	.catch(err => {
-// 		console.error(err)
-// 	})
-// }
-
 // express
 let location = '/public';
 let express = require('express');
 let app = express();
 let serv = require('http').Server(app);
 app.get('/',function(req, res) {
-	// res.send("Sorry, game is temporarily unavailable due to database service shutdown")
+	// res.send("Temp Text")
 	res.sendFile(__dirname + location + '/index.html');
 });
 app.use(location, express.static(__dirname + location));
@@ -53,8 +35,7 @@ serv.listen(port);
 // socket
 let state = 0;
 let d = false;
-// let timer = 45000;
-let timer = 5000;
+let timer = 45000;
 let SOCKET_LIST = {};
 let io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket) {
@@ -94,32 +75,6 @@ io.sockets.on('connection', function(socket) {
 		}).catch(err => {
 			console.error(err)
 		})
-
-
-		// db.accounts.find({username:data.user, password:data.pass}, function(err, res) {
-		//   let taken = false;
-		//   if (PLAYER_LIST.length != 0) {
-		// 	for (let i in PLAYER_LIST) {
-		// 	  if (PLAYER_LIST[i].user == data.user) {
-		// 		taken = true;
-		// 		socket.emit('signInResponse', {success: 2}); // user taken
-		// 		break;
-		// 	  }
-		// 	}
-		//   }
-		//   let succ = res.length > 0;
-		//   if (succ && !taken) {
-		// 	socket.emit('signInResponse', {success:1}); // successful login
-		// 	if (state == 2 && countProperties(PLAYER_LIST) + 1 >= 2) timer = 10000;
-		// 	createPlayer(socket.id, data.user);
-		// 	for (let i in SOCKET_LIST) {
-		// 	  SOCKET_LIST[i].emit('addToChat', PLAYER_LIST[socket.id].user + ' has joined the game.');
-		// 	}
-		//   }
-		//   else if (!succ && !taken) {
-		// 	socket.emit('signInResponse', {success:0}); // wrong user and pass
-		//   }
-		// });
 	});
 
 	socket.on('register', async function(data) {
@@ -157,25 +112,6 @@ io.sockets.on('connection', function(socket) {
 			}
 
 		}
-
-		// db.accounts.find({username:data.user}, function(err, res) {
-		//   let exists = (res.length > 0);
-		//   let empty = (data.user == "" || data.pass == "");
-		//   let longer = (data.user.length >= 14);
-		//   if (longer) {
-		// 	socket.emit('registerResponse', {success:4}); // user too long
-		//   }
-		//   else if (!exists && !empty) {
-		// 	socket.emit('registerResponse', {success:1}); // successful register
-		// 	db.accounts.insert({username:data.user, password:data.pass, highScore:0, scores:[], avgScore:0});
-		//   }
-		//   else if (!exists && empty) {
-		// 	socket.emit('registerResponse', {success:0}); // empty user or pass
-		//   }
-		//   else if (exists) {
-		// 	socket.emit('registerResponse', {success:2}); // user taken
-		//   }
-		// });
 	});
 
 	socket.on('leaderboard', async function(id) {
@@ -193,13 +129,6 @@ io.sockets.on('connection', function(socket) {
 		}).catch(err => {
 			console.error(err)
 		})
-
-		// db.accounts.find(function(err, res) {
-		//   for (let i in res) {
-		// 	leader.push({username: res[i].username, avgScore: res[i].avgScore, highScore: res[i].highScore});
-		//   }
-		//   socket.emit("leaderboardResponse", leader);
-		// });
 	});
 
 	socket.on('sendMsgToServer', function(data) {
@@ -284,8 +213,7 @@ function waitForPlayers() {
 function startGame() {
   final = null;
   finala = null;
-  // timer = 45000;
-  timer = 5000;
+  timer = 45000;
   let count = 1;
   let numPlayers = countProperties(PLAYER_LIST);
   for (let i in PLAYER_LIST) {
@@ -300,52 +228,32 @@ function startGame() {
 }
 
 async function endGame() {
-	console.log("end game");
 	state = 3;
 	timer = 10000;
   for (let i in PLAYER_LIST) {
-	if (!PLAYER_LIST[i].spectating) {
-		Account.updateOne(
-			{ username: PLAYER_LIST[i].user },
-			{ $push: {scores: PLAYER_LIST[i].score} },
-		(err, num, raw) => {})
-		let entry
-		let totalScores = 0
-		await Account.find({
-			username: PLAYER_LIST[i].user
-		}).then(entries => {
-			entry = entries[0]
-			for (let i = 0; i < entry.scores.length; i++) {
-				totalScores += entry.scores[i];
-			}
-		}).catch(err => {
-			console.error(err)
-		})
-		console.log(entry);
-		Account.updateOne(
-			{ username: PLAYER_LIST[i].user },
-			{ avgscore: (totalScores / entry.scores.length).toFixed(2),
-			highscore: Math.max(entry.highscore, PLAYER_LIST[i].score)},
+		if (!PLAYER_LIST[i].spectating) {
+			Account.updateOne(
+				{ username: PLAYER_LIST[i].user },
+				{ $push: {scores: PLAYER_LIST[i].score} },
 			(err, num, raw) => {})
-
-		// if (PLAYER_LIST[i].score > entry.highscore) {
-		// 	await Account.updateOne(
-		// 		{ username: PLAYER_LIST[i].user },
-		// 		{ highscore: PLAYER_LIST[i].score },
-		// 		(err, num, raw) => {})
-		// }
-	  // db.accounts.update({"username":PLAYER_LIST[i].user}, {$push: {"scores":PLAYER_LIST[i].score}});
-	  // db.accounts.find({"username":PLAYER_LIST[i].user}, function(err, res) {
-	  //   for (let i = 0; i < res[0].scores.length; i++) {
-	  //     totalScores += res[0].scores[i];
-	  //   }
-	  //   db.accounts.update({"username":PLAYER_LIST[i].user}, {$set: {"avgScore":(totalScores / res[0].scores.length).toFixed(2)}});
-	  //   totalScores = 0;
-	  //   if (PLAYER_LIST[i].score > res[0].highScore) {
-	  //     db.accounts.update({"username":PLAYER_LIST[i].user}, {$set: {"highScore":PLAYER_LIST[i].score}});
-	  //   }
-	  // });
-	}
+			let entry
+			let totalScores = 0
+			await Account.find({
+				username: PLAYER_LIST[i].user
+			}).then(entries => {
+				entry = entries[0]
+				for (let i = 0; i < entry.scores.length; i++) {
+					totalScores += entry.scores[i];
+				}
+			}).catch(err => {
+				console.error(err)
+			})
+			Account.updateOne(
+				{ username: PLAYER_LIST[i].user },
+				{ avgscore: (totalScores / entry.scores.length).toFixed(2),
+				highscore: Math.max(entry.highscore, PLAYER_LIST[i].score)},
+				(err, num, raw) => {})
+		}
   }
 }
 
